1e755030ab0069e792b99c30ec7b271dea60b087
Valery Alikin <weblcw@gmail.com>
2018-04-16 20:11:53 +0500

    fix song progress

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,44 @@
-
-$(document).ready(function(){
+$(document).ready(function () {
     var audioElement = document.createElement('audio');
     audioElement.setAttribute('src', $('.active-song').attr('data-src'));
 
     var tl = new TimelineMax();
-    tl.to('.player__albumImg', 3, {rotation: '360deg', repeat:-1, ease: Power0.easeNone}, '-=0.2');    
+    tl.to('.player__albumImg', 3, {
+        rotation: '360deg',
+        repeat: -1,
+        ease: Power0.easeNone
+    }, '-=0.2');
     tl.pause();
 
-    $('.player__play').click(function() {
-        
-        if($('.player').hasClass('play')) {
+    $('.player__play').click(function () {
+
+        if ($('.player').hasClass('play')) {
             $('.player').removeClass('play');
             audioElement.pause();
-            TweenMax.to('.player__albumImg', 0.2, {scale: 1,ease: Power0.easeNone})
+            TweenMax.to('.player__albumImg', 0.2, {
+                scale: 1,
+                ease: Power0.easeNone
+            })
             tl.pause();
         } else {
             $('.player').addClass('play');
             audioElement.play();
-            TweenMax.to('.player__albumImg', 0.2, {scale: 1.1,ease: Power0.easeNone})
-            tl.resume();            
+            TweenMax.to('.player__albumImg', 0.2, {
+                scale: 1.1,
+                ease: Power0.easeNone
+            })
+            tl.resume();
         }
-        
+
     });
 
 
     var playhead = document.getElementById("playhead");
-    audioElement.addEventListener("timeupdate", function(){ 
-        var duration = this.duration; 
-        var currentTime = this.currentTime; 
-        var percentage = (currentTime / duration) * 100; 
-        playhead.style.width = percentage * 4 + 'px'; 
+    audioElement.addEventListener("timeupdate", function () {
+        var duration = this.duration;
+        var currentTime = this.currentTime;
+        var percentage = (currentTime / duration) * 100;
+        playhead.style.width = percentage * 4 + 'px';
     });
 
     function updateInfo() {
@@ -38,24 +47,48 @@ $(document).ready(function(){
     }
     updateInfo();
 
-    $('.player__next').click(function() {        
-        if($('.player .player__albumImg.active-song').is(':last-child')) {
+    $('.player__next').click(function () {
+        if ($('.player .player__albumImg.active-song').is(':last-child')) {
             $('.player__albumImg.active-song').removeClass('active-song');
             $('.player .player__albumImg:first-child').addClass('active-song');
+            audioElement.addEventListener("timeupdate", function () {
+                var duration = this.duration;
+                var currentTime = this.currentTime;
+                var percentage = (currentTime / duration) * 100;
+                playhead.style.width = percentage * 4 + 'px';
+            });
         } else {
             $('.player__albumImg.active-song').removeClass('active-song').next().addClass('active-song');
+            audioElement.addEventListener("timeupdate", function () {
+                var duration = this.duration;
+                var currentTime = this.currentTime;
+                var percentage = (currentTime / duration) * 100;
+                playhead.style.width = percentage + '%';
+            });
         }
         updateInfo();
         audioElement.setAttribute('src', $('.active-song').attr('data-src'));
         audioElement.play();
     });
 
-    $('.player__prev').click(function() {        
-        if($('.player .player__albumImg.active-song').is(':first-child')) {
+    $('.player__prev').click(function () {
+        if ($('.player .player__albumImg.active-song').is(':first-child')) {
             $('.player__albumImg.active-song').removeClass('active-song');
             $('.player .player__albumImg:last-child').addClass('active-song');
+            audioElement.addEventListener("timeupdate", function () {
+                var duration = this.duration;
+                var currentTime = this.currentTime;
+                var percentage = (currentTime / duration) * 100;
+                playhead.style.width = percentage * 4 + 'px';
+            });
         } else {
             $('.player__albumImg.active-song').removeClass('active-song').prev().addClass('active-song');
+            audioElement.addEventListener("timeupdate", function () {
+                var duration = this.duration;
+                var currentTime = this.currentTime;
+                var percentage = (currentTime / duration) * 100;
+                playhead.style.width = percentage * 4 + 'px';
+            });
         }
         updateInfo();
         audioElement.setAttribute('src', $('.active-song').attr('data-src'));
